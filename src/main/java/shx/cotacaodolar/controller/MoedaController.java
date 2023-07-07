388a7ee18fcb5ce91b95aacd55c188c97555220b
jonasegf <jonas.edu@hotmail.com>
2023-07-07 13:45:12 -0300

    Resolução

--- src/main/java/shx/cotacaodolar/controller/MoedaController.java
+++ src/main/java/shx/cotacaodolar/controller/MoedaController.java
@@ -1,44 +1,41 @@
 package shx.cotacaodolar.controller;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.text.ParseException;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import shx.cotacaodolar.model.Moeda;
 import shx.cotacaodolar.service.MoedaService;
-
 
 @RestController
 @RequestMapping(value = "/")
 public class MoedaController {
 
-    @Autowired
-    private MoedaService moedaService;
+  @Autowired
+  private MoedaService moedaService;
 
-    @GetMapping("/moeda/{data1}&{data2}")
-    public List<Moeda> getCotacoesPeriodo(@PathVariable("data1") String startDate, @PathVariable("data2") String endDate) throws IOException, MalformedURLException, ParseException{
-        return moedaService.getCotacoesPeriodo(startDate, endDate);
-    }
-    
-    // Desenvolver uma rotina que retorna a cotação atual
-    @GetMapping("/moeda/atual")
-    public List<Moeda> getCotacaoAtual() throws IOException, MalformedURLException, ParseException{
-        // return moedaService.getCotacaoAtual();
-        return null; // substituir pela chamada do método no service quando criado como está acima.
-    }
-    
-    // Desenvolver uma rotina que recebe um período e retorna uma lista de cotações, somente com as cotações menores que a cotação atual.
-    @GetMapping("/moeda/{data1}&{data2}")
-    public List<Moeda> getCotacoesMenoresAtual(@PathVariable("data1") String startDate, @PathVariable("data2") String endDate) throws IOException, MalformedURLException, ParseException{
-        // return moedaService.getCotacoesMenoresAtual(startDate, endDate);
-        return null; // substituir pela chamada do método no service quando criado como está acima.
-    }
+  @GetMapping("/moeda/{data1}&{data2}")
+  public ResponseEntity<List<Moeda>> getCotacoesPeriodo(
+      @PathVariable("data1") String startDate,
+      @PathVariable("data2") String endDate) throws IOException, ParseException {
+    return ResponseEntity.ok(moedaService.getCotacoesPeriodo(startDate, endDate));
+  }
+
+  @GetMapping("/moeda/atual")
+  public ResponseEntity<List<Moeda>> getCotacaoAtual() throws IOException, ParseException {
+    return ResponseEntity.ok(moedaService.getCotacaoAtual());
+  }
+
+  @GetMapping("/moeda/menorquehoje/{data1}&{data2}")
+  public ResponseEntity<List<Moeda>> getCotacoesMenoresAtual(
+      @PathVariable("data1") String startDate,
+      @PathVariable("data2") String endDate) throws IOException, ParseException {
+    return ResponseEntity.ok(moedaService.getCotacoesMenoresAtual(startDate, endDate));
+  }
 
 }
--- src/main/java/shx/cotacaodolar/service/MoedaService.java
+++ src/main/java/shx/cotacaodolar/service/MoedaService.java
@@ -1,58 +1,105 @@
 package shx.cotacaodolar.service;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
 import shx.cotacaodolar.model.Moeda;
 import shx.cotacaodolar.model.Periodo;
-
-
 
 @Service
 public class MoedaService {
 
-	// o formato da data que o método recebe é "MM-dd-yyyy"
-    public List<Moeda> getCotacoesPeriodo(String startDate, String endDate) throws IOException, MalformedURLException, ParseException{
-        Periodo periodo = new Periodo(startDate, endDate);
+  // o formato da data que o método recebe é "MM-dd-yyyy"
+  public List<Moeda> getCotacoesPeriodo(String startDate, String endDate)
+      throws IOException, ParseException {
+    Periodo periodo = new Periodo(startDate, endDate);
 
-        String urlString = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?%40dataInicial='" + periodo.getDataInicial() + "'&%40dataFinalCotacao='" + periodo.getDataFinal() + "'&%24format=json&%24skip=0&%24top=" + periodo.getDiasEntreAsDatasMaisUm();
+    String urlString =
+        "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?%40dataInicial='" +
+            periodo.getDataInicial() + "'&%40dataFinalCotacao='" + periodo.getDataFinal() +
+            "'&%24format=json&%24skip=0&%24top=" + periodo.getDiasEntreAsDatasMaisUm();
 
-        URL url = new URL(urlString);
-        HttpURLConnection request = (HttpURLConnection)url.openConnection();
-        request.connect();
+    var cotacoesArray = getJsonElements(urlString);
 
-        JsonElement response = JsonParser.parseReader(new InputStreamReader((InputStream)request.getContent()));
-        JsonObject rootObj = response.getAsJsonObject();
-        JsonArray cotacoesArray = rootObj.getAsJsonArray("value");
+    List<Moeda> moedasLista = new ArrayList<>();
 
-        List<Moeda> moedasLista = new ArrayList<Moeda>();
+    for (JsonElement obj : cotacoesArray) {
+      Moeda moedaRef = new Moeda();
+      Date data = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(
+          obj.getAsJsonObject().get("dataHoraCotacao").getAsString());
 
-        for(JsonElement obj : cotacoesArray){
-            Moeda moedaRef = new Moeda();
-            Date data = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(obj.getAsJsonObject().get("dataHoraCotacao").getAsString());
-
-            moedaRef.preco = obj.getAsJsonObject().get("cotacaoCompra").getAsDouble();
-            moedaRef.data = new SimpleDateFormat("dd/MM/yyyy").format(data);
-            moedaRef.hora = new SimpleDateFormat("HH:mm:ss").format(data);
-            moedasLista.add(moedaRef);
-        }
-        return moedasLista;
+      moedaRef.preco = obj.getAsJsonObject().get("cotacaoCompra").getAsDouble();
+      moedaRef.data = new SimpleDateFormat("dd/MM/yyyy").format(data);
+      moedaRef.hora = new SimpleDateFormat("HH:mm:ss").format(data);
+      moedasLista.add(moedaRef);
     }
+    return moedasLista;
+  }
+
+  public List<Moeda> getCotacaoAtual() throws IOException, ParseException {
+    var formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+    var dataAtual = formatter.format(LocalDate.now());
+
+    String urlString =
+        "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='" +
+            dataAtual + "'&$top=100&$format=json";
+
+    var cotacoesArray = getJsonElements(urlString);
+    if (cotacoesArray.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    Moeda moedaRef = new Moeda();
+    var moedaJson = cotacoesArray.get(0).getAsJsonObject();
+
+    Date data = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(
+        moedaJson.get("dataHoraCotacao").getAsString());
+
+    moedaRef.preco = moedaJson.get("cotacaoCompra").getAsDouble();
+    moedaRef.data = new SimpleDateFormat("dd/MM/yyyy").format(data);
+    moedaRef.hora = new SimpleDateFormat("HH:mm:ss").format(data);
+
+    return Collections.singletonList(moedaRef);
+  }
+
+  public List<Moeda> getCotacoesMenoresAtual(String startDate, String endDate)
+      throws IOException, ParseException {
+    var atual = getCotacaoAtual();
+    if (atual.isEmpty()) {
+      return Collections.emptyList();
+    }
+    var cotacaoPeriodo = getCotacoesPeriodo(startDate, endDate);
+
+    return cotacaoPeriodo.stream().filter(x -> x.preco < atual.get(0).preco)
+        .collect(Collectors.toList());
+  }
+
+  private static JsonArray getJsonElements(final String urlString) throws IOException {
+    URL url = new URL(urlString);
+    HttpURLConnection request = (HttpURLConnection) url.openConnection();
+    request.connect();
+
+    JsonElement response =
+        JsonParser.parseReader(new InputStreamReader((InputStream) request.getContent()));
+    JsonObject rootObj = response.getAsJsonObject();
+
+    return rootObj.getAsJsonArray("value");
+  }
 
 }
